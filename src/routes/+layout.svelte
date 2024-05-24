6ad4d8ede856e90bdbd315f1291cc1c7cd6123b9
aadtyaraj01 <aadtyaraj01@gmail.com>
2024-05-24 15:04:44 +0530

    skeleton next

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
-<script>
-  import "../app.css";
-  import Navbar from '$lib/components/Navbar.svelte'
-</script>
+
 
 <a href="/comp">Components 🧪</a>
 <a href="/test">Tests 🧑🏼‍💻</a>
 
 
-  <slot />
+  <slot></slot>
+<slot></slot><script>import "../app.css";
+import Navbar from '$lib/components/Navbar.svelte'</script><style></style>
